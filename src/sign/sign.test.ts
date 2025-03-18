921b5ce17a307bfea2dfc8e4222b2f4c7cbc9eba
Ethan Wessel <ethan.wessel@consensys.net>
2025-03-18 09:30:52 -0700

    chore: setup sign and test

--- src/sign/sign.test.ts
+++ src/sign/sign.test.ts
@@ -1,10 +1,19 @@
-import { sign } from './sign';
+import { sign, SigningAlgorithm } from './sign';
+const {
+    generateKeyPairSync,
+    createSign,
+    createVerify,
+  } = require('node:crypto');
 
 describe('sign', () => {
     it('should return a string', () => {
+        const { privateKey, publicKey } = generateKeyPairSync('rsa', {
+            modulusLength: 2048,
+        });
         const data = 'data';
-        const privateKey = 'privateKey';
-        const result = sign({ data, privateKey });
+        const result = sign({ data, privateKey, algorithm: SigningAlgorithm.RSA });
         expect(typeof result).toBe("string");
     });
 });
+
+  
--- src/sign/sign.ts
+++ src/sign/sign.ts
@@ -1,5 +1,20 @@
+import { createSign, getHashes } from "crypto";
+
+export enum SigningAlgorithm {
+    RSA = "RSA-SHA256",
+    RSA_PSS = "RSA-PSS",
+    ECDSA = "SHA256"
+}
+
+
 export function sign(
-    { data, privateKey }: { data: string, privateKey: string }
+    { data, privateKey, algorithm }: { data: string, privateKey: string, algorithm: SigningAlgorithm }
 ): string {
-    return "Not implemented";
+    // verify that the algorithm is supported
+    if (!getHashes().includes(algorithm)) {
+        throw new Error(`Algorithm ${algorithm} is not supported`);
+    }
+    const signer = createSign(algorithm);
+    signer.update(data);
+    return signer.sign(privateKey, "hex");
 }
