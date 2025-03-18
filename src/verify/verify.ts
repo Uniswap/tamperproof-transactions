84bb080fa6b37c187d8d6cde75650cc7342dc382
mr <morgan.roman@uniswap.org>
2025-03-18 13:35:49 -0400

    broken due to type errors

--- src/verify/verify.ts
+++ src/verify/verify.ts
@@ -1,21 +1,46 @@
 import { createSign, createVerify, KeyObject } from "crypto";
 import { SigningAlgorithm } from "../algorithms";
 
-export async function verifyAsync(
-    {
-        calldata, signature, algorithm, url
-    }: {
-        calldata: string, signature: string, algorithm: SigningAlgorithm, url?: string
-    }
-): Promise<boolean> {
-    return false
-    // const publicKey = await fetch(url).then(res => res.json());
+import { resolveTxt } from "dns";
 
-    // // using public key, verify the signature
-    // const verify = createVerify(algorithm);
-    // verify.update(calldata);
-    // verify.end();
-    // return verify.verify(publicKey, signature, "hex");
+
+export const PREFIX = "TWIT=";
+
+export async function verifyAsyncDns(
+    calldata: string, signature: string, host: string, id?: number
+): Promise<boolean> {
+    let txtRecords: string[] = [];
+    resolveTxt(
+        host,
+        res => {
+            txtRecords = res as string[];
+        }
+    );
+
+    let twistRecord: string | undefined;
+    txtRecords.forEach(record => {
+        if (record.startsWith(PREFIX)) {
+            twistRecord = record;
+        }
+    });
+
+    if (!twistRecord) {
+        throw new Error(`No TXT record found for host ${host}`);
+    }
+
+    return verifyAsyncJson(calldata, signature, twistRecord.slice(PREFIX.length)[0], id);
+
+    // const publicKey = await fetch(url).then(res => res.json());
+    // return verifySync(calldata, signature, publicKey.algorithm, publicKey.key);
+}
+
+export async function verifyAsyncJson(
+    calldata: string, signature: string, url: string, id?: number
+): Promise<boolean> {
+    const publicKeys = await fetch(url).then(res => res.publicKeys);
+    const publicKey = id ? publicKeys[id] : publicKeys[0];
+
+    return verifySync(calldata, signature, publicKey.algorithm, publicKey.key);
 }
 
 export function verifySync(
