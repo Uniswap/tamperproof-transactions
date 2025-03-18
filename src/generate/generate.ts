f357bbfdfd0120f54493cfd33d7855fae9d8719e
Ethan Wessel <ethan.wessel@consensys.net>
2025-03-18 10:20:45 -0700

    chore: generate and generate test

--- src/generate/generate.ts
+++ src/generate/generate.ts
@@ -1,21 +1,16 @@
 import { SigningAlgorithm } from "../algorithms";
 export type PublicKey = {
     key: string,
-    algorithm: string
+    algorithm: SigningAlgorithm
 }
 
 export function generate(...publicKeys: PublicKey[]): string {
     let pubKeys: object[] = publicKeys.map((publicKey, index) => {
-        if (publicKey.algorithm in SigningAlgorithm) {
-            return {
-                id: index,
-                alg: publicKey.algorithm,
-                key: publicKey.key
-            }
-        } else {
-            throw "Invalid algorithm";
+        return {
+            id: index,
+            alg: publicKey.algorithm,
+            publicKey: publicKey.key
         }
-
     });
     return JSON.stringify(
         {
