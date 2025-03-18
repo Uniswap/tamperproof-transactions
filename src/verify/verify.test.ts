9275571efc53c26694d679dffd137bd730e513c2
mr <morgan.roman@uniswap.org>
2025-03-18 12:35:32 -0400

    modifications

--- src/verify/verify.test.ts
+++ src/verify/verify.test.ts
@@ -1,4 +1,6 @@
 import { verify } from './verify';
+import { SigningAlgorithm } from '../algorithms';
+
 
 describe('verify', () => {
     test('verify returns false', () => {
@@ -6,6 +8,13 @@ describe('verify', () => {
             calldata: '0x',
             signature: '0x',
             url: 'https://example.com',
+        })).toBe(false);
+    });
+
+    test('verify returns false', () => {
+        expect(verify({
+            calldata: '0x',
+            signature: '0x',
             publickey: '0x'
         })).toBe(false);
     });
