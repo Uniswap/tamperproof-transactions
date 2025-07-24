e4dcd372570f68517937c4c2c87cfb706a9ad42c
David Gilman <david.gilman@uniswap.org>
2025-07-24 11:53:52 -0400

    Added linting

--- src/generate/generate.test.ts
+++ src/generate/generate.test.ts
@@ -3,15 +3,15 @@ import { SigningAlgorithm } from '../algorithms';
 import { generate } from './generate';
 
 describe('generate', () => {
-    it('should return a string', () => {
-        const { publicKey } = generateKeyPairSync('rsa', {
-            modulusLength: 2048,
-        });
-        expect(typeof generate(
-            {
-                key: publicKey.export({ type: 'spki', format: 'der' }).toString('hex'),
-                algorithm: SigningAlgorithm.RSA
-            }
-        )).toBe("string");
+  it('should return a string', () => {
+    const { publicKey } = generateKeyPairSync('rsa', {
+      modulusLength: 2048,
     });
+    expect(
+      typeof generate({
+        key: publicKey.export({ type: 'spki', format: 'der' }).toString('hex'),
+        algorithm: SigningAlgorithm.RSASSA_PKCS1_v1_5,
+      })
+    ).toBe('string');
+  });
 });
