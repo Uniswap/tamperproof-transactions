381e9539608f9453af4420ae5bd461164920cd8f
David Gilman <david.gilman@uniswap.org>
2025-07-24 11:53:52 -0400

    Added linting

--- src/algorithms.ts
+++ src/algorithms.ts
@@ -1,22 +1,24 @@
 export const SIGNING_ALGORITHMS = {
-    "ECDSA": {
-        name: "ECDSA",
-        hash: {name: "SHA-256"},
-      },
-    "Ed25519": { name: "Ed25519" },
-    "RSA-PSS": {
-        name: "RSA-PSS",
-        hash: {name: "SHA-256"},
-        saltLength: 32,
-    },
-    "RSASSA-PKCS1-v1_5": {
-        name: "RSASSA-PKCS1-v1_5",
-    },
-    "Ed448": {
-        name: "Ed448",
-    }
+  ECDSA: {
+    name: 'ECDSA',
+    hash: { name: 'SHA-256' },
+  },
+  Ed25519: { name: 'Ed25519' },
+  'RSA-PSS': {
+    name: 'RSA-PSS',
+    hash: { name: 'SHA-256' },
+    saltLength: 32,
+  },
+  'RSASSA-PKCS1-v1_5': {
+    name: 'RSASSA-PKCS1-v1_5',
+  },
+  Ed448: {
+    name: 'Ed448',
+  },
 } as const;
 
-export function isSigningAlgorithm(maybe: string): maybe is keyof typeof SIGNING_ALGORITHMS {
-    return maybe in SIGNING_ALGORITHMS;
+export function isSigningAlgorithm(
+  maybe: string
+): maybe is keyof typeof SIGNING_ALGORITHMS {
+  return maybe in SIGNING_ALGORITHMS;
 }
