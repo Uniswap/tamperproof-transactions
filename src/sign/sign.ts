bd672882788a4fc1ed20ff11f64fa3bd9dc2252f
David Gilman <david.gilman@uniswap.org>
2025-07-24 11:53:52 -0400

    Added linting

--- src/sign/sign.ts
+++ src/sign/sign.ts
@@ -1,17 +1,23 @@
-import { webcrypto } from "crypto";
-import { SIGNING_ALGORITHMS, isSigningAlgorithm } from "../algorithms";
+import { webcrypto } from 'crypto';
+import { SIGNING_ALGORITHMS, isSigningAlgorithm } from '../algorithms';
 
 export async function sign(
-    data: string, privateKey: webcrypto.CryptoKey, algorithm: keyof typeof SIGNING_ALGORITHMS
+  data: string,
+  privateKey: webcrypto.CryptoKey,
+  algorithm: keyof typeof SIGNING_ALGORITHMS
 ): Promise<string> {
-    // verify that the algorithm is supported
-    if (!isSigningAlgorithm(algorithm)) {
-        throw new Error(`Algorithm ${algorithm} is not supported`);
-    }
+  // verify that the algorithm is supported
+  if (!isSigningAlgorithm(algorithm)) {
+    throw new Error(`Algorithm ${algorithm as string} is not supported`);
+  }
 
-    const encoder = new TextEncoder();
-    const bufferData = encoder.encode(data);
+  const encoder = new TextEncoder();
+  const bufferData = encoder.encode(data);
 
-    const signature = await webcrypto.subtle.sign(SIGNING_ALGORITHMS[algorithm], privateKey, bufferData);
-    return Buffer.from(signature).toString('hex');
+  const signature = await webcrypto.subtle.sign(
+    SIGNING_ALGORITHMS[algorithm],
+    privateKey,
+    bufferData
+  );
+  return Buffer.from(signature).toString('hex');
 }
