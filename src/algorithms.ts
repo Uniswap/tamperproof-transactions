4da00cb223f9ead9229cd121c04ae33b274a0732
David Gilman <david.gilman@uniswap.org>
2025-07-24 11:45:04 -0400

    Replace signing crypto with crypto.subtle

--- src/algorithms.ts
+++ src/algorithms.ts
@@ -1,5 +1,22 @@
-export enum SigningAlgorithm {
-    RSA = "RSA-SHA256",
-    RSA_PSS = "RSA-PSS",
-    ECDSA = "SHA256"
+export const SIGNING_ALGORITHMS = {
+    "ECDSA": {
+        name: "ECDSA",
+        hash: {name: "SHA-256"},
+      },
+    "Ed25519": { name: "Ed25519" },
+    "RSA-PSS": {
+        name: "RSA-PSS",
+        hash: {name: "SHA-256"},
+        saltLength: 32,
+    },
+    "RSASSA-PKCS1-v1_5": {
+        name: "RSASSA-PKCS1-v1_5",
+    },
+    "Ed448": {
+        name: "Ed448",
+    }
+} as const;
+
+export function isSigningAlgorithm(maybe: string): maybe is keyof typeof SIGNING_ALGORITHMS {
+    return maybe in SIGNING_ALGORITHMS;
 }
